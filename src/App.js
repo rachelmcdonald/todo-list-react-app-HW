5af5cfb47f6c9cf54bbb4bb8e1f7464970c1a3b7
Rachel McDonald <rach-mcdonald@outlook.com>
2022-02-21 17:46:47 +0000

    Week 7, Day 1 homework to create a React app that allows the user to add a new task and then be able to change its priority level to high upon clicking a button. Feedback will be displayed on the browser to inform user that the input was successful

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+ const [tasks, setTasks] = useState([
+   {name: "Feed Ghost", priority: "high"},
+   {name: "Play Animal Crossing", priority: "low"},
+   {name: "Complete Homework", priority: "high"}
+ ])
+ const [newTask, setNewTask] = useState("")
+
+ const prioritiseTask = (index) => {
+   tasks[index].isHigh = true
+   const copyOfTasks = [...tasks]
+   setTasks(copyOfTasks)
+ }
+
+ const taskNodes = tasks.map( (task, index) => {
+   return(
+     <li key={index} className={task.isHigh ? "high" : "not-high"}>
+       <span>{task.name}</span>
+       {task.isHigh ? <span className="high">&#10071; High Priority</span>:
+       <button onClick={ () => {prioritiseTask(index)} }
+       >Make High Priority</button> }
+     </li>
+   )
+ })
+
+ const handleTaskInput = (event) => {
+   setNewTask(event.target.value)
+ }
+
+ const saveNewTask = (event) => {
+   event.preventDefault()
+   const newTaskItem = {name: newTask, isHigh: false}
+   const copyOfTasks = [...tasks, newTaskItem]
+   setTasks(copyOfTasks)
+   setNewTask("")
+ }
+
+ return (
+   <div className="App">
+     <h1>Rachel's To-Do List &#128221;</h1>
+     <hr />
+
+     <ul>
+       {taskNodes}
+     </ul>
+
+     <form onSubmit={saveNewTask}>
+       <label htmlFor="new-task">Add a new task</label>
+       <input type="text" id="new-task" value={newTask} onChange={handleTaskInput}/>
+       <input type="submit" value="Save New Task" />
+     </form>
+   </div>
+ )
 }
 
 export default App;
